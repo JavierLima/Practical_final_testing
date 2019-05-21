6d742866d7b70f26834e77712853e5579669b77a
JavierLima <40237259+JavierLima@users.noreply.github.com>
2019-05-21 21:24:46 +0200

    modificacion en tests_word_counter

--- test_word_counter.py
+++ test_word_counter.py
@@ -86,5 +86,7 @@ def test_word_empty_text():
     assert word_counter("") == None
 
 if __name__ == '__main__':
+    import nltk
+    nltk.download('stopwords')
     test_word_counter_spanish()
     test_word_counter_first_input_integer()
