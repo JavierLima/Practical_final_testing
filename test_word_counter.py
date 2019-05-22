b487ccd07b884e3767cc03bd6ccaf12480ce5186
JavierLima <40237259+JavierLima@users.noreply.github.com>
2019-05-22 11:45:35 +0200

    probando unittest

--- test_word_counter.py
+++ test_word_counter.py
@@ -6,7 +6,7 @@ import unittest
 from twitter_word_counter import word_counter
 
 class KnownValues(unittest.TestCase):
-    def test_word_counter_spanish():
+    def test_word_counter_spanish(self):
         self.assertEqual(word_counter("Vamos a llevarnos bien, porque si no va a haber hondonadas de ostias aqui",'spanish'),({'vamos': 1, 'llevarnos': 1,
                                                                                                                    'bien': 1, 'si': 1, 'va': 1,
                                                                                                                    'haber': 1, 'hondonadas': 1,
