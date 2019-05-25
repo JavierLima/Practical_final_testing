da61001c5f569abf278aae2c405279606b701eb1
JavierLima <40237259+JavierLima@users.noreply.github.com>
2019-05-26 00:02:44 +0200

    debugging web

--- code_folder/tests/features/environment.py
+++ code_folder/tests/features/environment.py
@@ -7,7 +7,7 @@ def before_scenario(context,scenario):
     options.add_argument("--headless")
     options.add_argument("--no-sandbox")
     context.driver = webdriver.Chrome("driver/chromedriver",options= options)
-    context.driver.implicitly_wait(1)
+    context.driver.implicitly_wait(10)
 
 def after_scenario(context,scenario):
     context.driver.close()
