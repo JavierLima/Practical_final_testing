2c6f5ccc181fba0a023fa759b6c0b726ed85d31c
JavierLima <40237259+JavierLima@users.noreply.github.com>
2019-05-25 20:38:59 +0200

    chrome options

--- code_folder/tests/features/environment.py
+++ code_folder/tests/features/environment.py
@@ -4,7 +4,8 @@ from selenium.webdriver.chrome.options import Options
 
 def before_scenario(context,scenario):
     options = Options()
-    options.add_argument("--start-maximized")
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
     context.driver = webdriver.Chrome("driver/chromedriver",options= options)
     context.driver.implicitly_wait(1)
 
