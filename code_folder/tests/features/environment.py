176ff96aac107ecd7a1ae9447ea90a8b4fe389b1
JavierLima <40237259+JavierLima@users.noreply.github.com>
2019-05-25 19:20:09 +0200

    cambiar unzip y carpeta de archivos

--- code_folder/tests/features/environment.py
+++ code_folder/tests/features/environment.py
@@ -5,7 +5,7 @@ from selenium.webdriver.chrome.options import Options
 def before_scenario(context,scenario):
     options = Options()
     options.add_argument("--start-maximized")
-    context.driver = webdriver.Chrome("/home/travis/JavierLima/Practical_final_testing/chromedriver",options= options)
+    context.driver = webdriver.Chrome("driver/chromedriver",options= options)
     context.driver.implicitly_wait(1)
 
 def after_scenario(context,scenario):
