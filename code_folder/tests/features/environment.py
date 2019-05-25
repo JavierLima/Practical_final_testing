7da4431d33711f21a34bbbf7f13fc1766d46d084
JavierLima <40237259+JavierLima@users.noreply.github.com>
2019-05-26 00:54:51 +0200

    opciones chromedriver modificadas

--- code_folder/tests/features/environment.py
+++ code_folder/tests/features/environment.py
@@ -3,11 +3,11 @@ from selenium.webdriver.chrome.options import Options
 
 
 def before_scenario(context,scenario):
-    #options = Options()
-    #options.add_argument("--headless")
-    #options.add_argument("--no-sandbox")
-    #options.add_argument("--disable-gpu")
-    context.driver = webdriver.Chrome("driver/chromedriver")
+    options = Options()
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    context.driver = webdriver.Chrome("driver/chromedriver", options=options)
 
 def after_scenario(context,scenario):
     context.driver.close()
