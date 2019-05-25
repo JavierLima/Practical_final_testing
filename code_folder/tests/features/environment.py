052fd36878abcbe07e80616a4a47600c28ec4fe0
JavierLima <40237259+JavierLima@users.noreply.github.com>
2019-05-25 22:50:02 +0200

    añadir chromdriver version

--- code_folder/tests/features/environment.py
+++ code_folder/tests/features/environment.py
@@ -6,7 +6,7 @@ def before_scenario(context,scenario):
     options = Options()
     options.add_argument("--headless")
     options.add_argument("--no-sandbox")
-    context.driver = webdriver.Chrome("/usr/lib/chromium-browser/chromedriver",options= options)
+    context.driver = webdriver.Chrome("tests/driver/chromedriver",options= options)
     context.driver.implicitly_wait(1)
 
 def after_scenario(context,scenario):
