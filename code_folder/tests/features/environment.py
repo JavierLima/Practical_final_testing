1b95d468ea915c832f0137225194bcb25cd764a4
JavierLima <40237259+JavierLima@users.noreply.github.com>
2019-05-26 00:45:33 +0200

    añadir opciones chromedriver

--- code_folder/tests/features/environment.py
+++ code_folder/tests/features/environment.py
@@ -4,9 +4,9 @@ from selenium.webdriver.chrome.options import Options
 
 def before_scenario(context,scenario):
     options = Options()
-    options.add_argument("--headless")
-    options.add_argument("--no-sandbox")
-    options.add_argument("--disable-gpu")
+    #options.add_argument("--headless")
+    #options.add_argument("--no-sandbox")
+    #options.add_argument("--disable-gpu")
     context.driver = webdriver.Chrome("driver/chromedriver",options= options)
 
 def after_scenario(context,scenario):
