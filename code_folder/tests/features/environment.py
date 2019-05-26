820bccb109153b7cfbea14705194a92260b0226b
JavierLima <40237259+JavierLima@users.noreply.github.com>
2019-05-26 16:25:27 +0200

    implicit wait

--- code_folder/tests/features/environment.py
+++ code_folder/tests/features/environment.py
@@ -8,8 +8,9 @@ def before_scenario(context,scenario):
     options.add_argument("--headless")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-gpu")
-#    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-dev-shm-usage")
     context.driver = webdriver.Chrome("driver/chromedriver", options=options)
+    context.implicitly_wait(5)
 
 def after_scenario(context,scenario):
     context.driver.close()
