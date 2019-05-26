6f0da2cacf2d53766638e325b43ef56f4ec49533
JavierLima <40237259+JavierLima@users.noreply.github.com>
2019-05-26 12:46:04 +0200

    binary location

--- code_folder/tests/features/environment.py
+++ code_folder/tests/features/environment.py
@@ -4,9 +4,11 @@ from selenium.webdriver.chrome.options import Options
 
 def before_scenario(context,scenario):
     options = Options()
+    options.binary_location = '/usr/bin/google-chrome-stable'
     options.add_argument("--headless")
     options.add_argument("--no-sandbox")
-    options.add_argument("--disable-dev-shm-usage")
+    otions.add_argument("--disable-gpu")
+#    options.add_argument("--disable-dev-shm-usage")
     context.driver = webdriver.Chrome("driver/chromedriver", options=options)
 
 def after_scenario(context,scenario):
