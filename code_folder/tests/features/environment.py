9767a42dcf84edacb00b9ac484f5f7bc60a3f036
JavierLima <40237259+JavierLima@users.noreply.github.com>
2019-05-26 17:50:31 +0200

    change enviroment options

--- code_folder/tests/features/environment.py
+++ code_folder/tests/features/environment.py
@@ -9,6 +9,7 @@ def before_scenario(context,scenario):
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-gpu")
     options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("window-size=1200,1100")
     context.driver = webdriver.Chrome("driver/chromedriver", options=options)
     context.driver.implicitly_wait(5)
 
