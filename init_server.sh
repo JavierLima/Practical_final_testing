2aed0c3663aa96ad0ae911f0c6528a9ce3324119
JavierLima <40237259+JavierLima@users.noreply.github.com>
2019-05-29 09:14:42 +0200

    preparacion de la rama master

--- init_server.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-python web/manage.py runserver
--- web/web/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-
-class NameForm(forms.Form):
-    your_name = forms.CharField(label='Username', max_length=100)
-
-	
-
